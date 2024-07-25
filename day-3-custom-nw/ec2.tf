ccd2dab9a1b69fb2524ba21810db727d7ee6253f
Philiparrolla <philiparrolla47@gmail.com>
2024-07-25 11:38:58 +0530

    Update ec2.tf

--- day-3-custom-nw/ec2.tf
+++ day-3-custom-nw/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "dev" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    subnet_id = aws_subnet.dev.id
     associate_public_ip_address = true
     security_groups = [ aws_security_group.dev.id ]
  
