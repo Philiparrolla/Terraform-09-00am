b6673b9699e70f549f2d3ce0fab3748674300ee1
Philiparrolla <philiparrolla47@gmail.com>
2024-07-25 11:50:19 +0530

    Update ec2.tf

--- day-3-custom-nw/ec2.tf
+++ day-3-custom-nw/ec2.tf
@@ -3,8 +3,6 @@ resource "aws_instance" "dev" {
     instance_type = var.instance_type
     key_name = var.key_name
     associate_public_ip_address = true
-    security_groups = [ aws_security_group.dev.id ]
- 
     tags = {
       Name = "dev_ec2"
     }
